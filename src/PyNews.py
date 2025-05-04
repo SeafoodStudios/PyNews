bfc034d83c6e5109e82f5ea9ac26bb96687cb56f
SeafoodStudios <williamluqilin@gmail.com>
2025-05-04 12:22:10 -0400

    Update PyNews.py

--- src/PyNews.py
+++ src/PyNews.py
@@ -13,4 +13,4 @@ print(response.text)
 
 @app.route("/")
 def display():
-    return Response("PyNews scrapes from CNN Lite, and it summarizes the news using Google Gemini. Here is the documentation: https://github.com/SeafoodStudios/PyNews\n\n" + str(response.text), mimetype='text/plain')
+    return Response("PyNews is a free news service that scrapes from CNN Lite, and summarizes the news using Google Gemini. Here is the documentation: https://github.com/SeafoodStudios/PyNews\n\n" + str(response.text), mimetype='text/plain')
